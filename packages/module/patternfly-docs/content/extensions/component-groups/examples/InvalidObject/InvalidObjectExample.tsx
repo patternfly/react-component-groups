5d53c047da2708fc0b977bee771063fc0f20fe18
InsaneZein <zsleiman@redhat.com>
2023-10-19 08:28:10 -0500

    Update packages/module/patternfly-docs/content/extensions/component-groups/examples/InvalidObject/InvalidObjectExample.tsx
    Co-authored-by: Filip Hlavac <50696716+fhlavac@users.noreply.github.com>

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/InvalidObject/InvalidObjectExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/InvalidObject/InvalidObjectExample.tsx
@@ -1,10 +1,6 @@
 import InvalidObject from '@patternfly/react-component-groups/dist/dynamic/InvalidObject';
 import React from 'react';
 
-const InvalidObjectExample: React.FunctionComponent = () => (
-  <>
-    <InvalidObject />
-  </>
-);
+const InvalidObjectExample: React.FunctionComponent = () => <InvalidObject />;
 
 export default InvalidObjectExample;
