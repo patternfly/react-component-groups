63ebd3160dafebae1de2fffcb9b50e2fa4a18663
InsaneZein <zsleiman@redhat.com>
2023-10-19 08:27:50 -0500

    Update packages/module/src/InvalidObject/index.ts
    Co-authored-by: Filip Hlavac <50696716+fhlavac@users.noreply.github.com>

--- packages/module/src/InvalidObject/index.ts
+++ packages/module/src/InvalidObject/index.ts
@@ -1,3 +1,2 @@
 export { default } from './InvalidObject';
-export { default as InvalidObject } from './InvalidObject';
-export { default as Icon404 } from '../NotFoundIcon/NotFoundIcon';
+export * from './InvalidObject';
