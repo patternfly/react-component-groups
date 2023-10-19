0bcc92f5f82bf47630bfe2404347cc31b2db3fa3
Zein Sleiman <zsleiman@redhat.com>
2023-10-19 08:56:50 -0500

    remove unncessary styling

--- packages/module/src/InvalidObject/InvalidObject.tsx
+++ packages/module/src/InvalidObject/InvalidObject.tsx
@@ -2,22 +2,6 @@ import { Button, EmptyState, EmptyStateBody, EmptyStateFooter, EmptyStateHeader,
 
 import NotFoundIcon from '../NotFoundIcon/NotFoundIcon';
 import React from 'react';
-import clsx from 'clsx';
-import { createUseStyles } from 'react-jss'
-
-const useStyles = createUseStyles({
-  "invalidObject": {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    textAlign: "center",
-    h1: { marginBottom: "30px" },
-    svg: { marginBottom: "30px" },
-    button: { fontSize: "20px" }
-  },
-  "invalidObjectSorryText": { maxWidth: "70%" },
-});
 
 export interface InvalidObjectProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
   /** Custom landing page button URL */
@@ -31,10 +15,8 @@ const InvalidObject: React.FunctionComponent<InvalidObjectProps> = ({
   toLandingPageUrl = `${window.location.origin}`,
   toLandingPageText = 'Return to homepage'
 }: InvalidObjectProps) => {
-  const classes = useStyles();
-  const invalidObjectClasses = clsx("pf-v5-l-page__main-section", "pf-v5-c-page__main-section", classes.invalidObject)
   return (
-    <EmptyState variant={EmptyStateVariant.full} className={invalidObjectClasses}>
+    <EmptyState variant={EmptyStateVariant.full}>
       <EmptyStateHeader titleText='We lost that page' icon={<NotFoundIcon />} headingLevel='h1' />
       <EmptyStateBody>Let&apos;s find you a new one. Try a new search or return home.</EmptyStateBody>
       <EmptyStateFooter>
