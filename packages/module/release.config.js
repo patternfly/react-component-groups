952190b620fd102db1b2fd9d5cfabf976e53bfef
Donald Labaj <dlabaj@redhat.com>
2023-10-26 13:36:52 -0400

    fix(prerelease): Updated tag to say prerelease.

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: [ { name: 'main', channel: 'prerelease' }, { name: 'v4', channel: 'alpha', prerelease: 'alpha' } ],
+  branches: [ 'do-not-delete', { name: 'main', channel: 'prerelease', prerelease: 'prerelease' } ],
   analyzeCommits: {
     preset: 'angular'
   },
@@ -9,6 +9,6 @@ module.exports = {
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'v${version}',
-  dryrun: true
+  tagFormat: 'prerelease-v${version}',
+  dryRun: true
 };
