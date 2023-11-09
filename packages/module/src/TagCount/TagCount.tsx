58e83d9dccffd64a2e76da334c4c9299cbd4c346
Filip Hlavac <filip.hlavac01@gmail.com>
2023-11-09 18:37:23 +0100

    fix(props): Add iconSize param to TagCount

--- packages/module/src/TagCount/TagCount.tsx
+++ packages/module/src/TagCount/TagCount.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { Button, ButtonProps, Icon } from '@patternfly/react-core';
+import { BaseSizes, Button, ButtonProps, Icon, IconComponentProps, IconProps } from '@patternfly/react-core';
 import { TagIcon } from '@patternfly/react-icons';
 import { createUseStyles } from 'react-jss'
 
@@ -21,17 +21,20 @@ export interface TagCountProps extends ButtonProps {
   count?: number;
   /** Additional classes added to the tag count component */
   className?: string;
+  /** Icon size */
+  iconSize?: 'sm' | 'md' | 'lg' | 'xl';
 }
 
 const TagCount: React.FunctionComponent<TagCountProps> = (
   { count, 
     className,
+    iconSize= 'md',
     ...props }: TagCountProps) => {
   const classes = useStyles();
   const tagClasses = clsx(classes.buttonTagCount, className);
   return (
     <Button aria-label="Tag count" {...props} variant="plain" isDisabled={!count} className={tagClasses}>
-      <Icon size="md">
+      <Icon iconSize={iconSize} >
         <TagIcon/>
       </Icon>
       <span className={classes.tagText}>{count}</span>
