9b09acef14ff98769d5ca80baa98a2d1ff8721e7
Filip Hlavac <filip.hlavac01@gmail.com>
2024-01-16 15:34:35 +0100

    Update tests

--- packages/module/src/ErrorState/ErrorState.test.tsx
+++ packages/module/src/ErrorState/ErrorState.test.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import ErrorState from './ErrorState';
 import { render, screen } from '@testing-library/react';
+import { Button } from '@patternfly/react-core';
 
 describe('ErrorState component', () => {
 
   it('should render correctly', () => {
-    render(<ErrorState errorTitle='A Basic Error' errorDescription='The following is an example of a basic error' />);
+    render(<ErrorState errorTitle='A Basic Error' errorDescription='The following is an example of a basic error' customFooter={<Button>Custom button</Button>} />);
 
     expect(screen.getByText('A Basic Error')).toBeVisible();
     expect(screen.getByText('The following is an example of a basic error')).toBeVisible();
-    expect(screen.getByText('Go to home page')).toBeVisible();
+    expect(screen.getByText('Custom button')).toBeVisible();
   });
 
   it('should render correctly with default props', () => {
