d0f53203c03c35ac05c2ab2aec2a175cc17d7f2f
Donald Labaj <dlabaj@redhat.com>
2024-03-05 14:20:09 -0500

    feat(cypress): Added code for cypress ci in package.json.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     viewportHeight: 1024,
     viewportWidth: 1400,
+    screenshotOnRunFailure: false,
+    reporter: "junit",
+    reporterOptions: {
+      mochaFile: "results/my-test-output-[hash].xml",
+      toConsole: true
+    },
   },
 });
