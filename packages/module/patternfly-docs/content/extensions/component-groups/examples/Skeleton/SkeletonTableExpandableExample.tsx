72067d71dcfd663c8de7f10a984f0921c6a32935
Cody Mitchell <comitche@redhat.com>
2024-05-16 18:12:14 -0400

    add isSelectable and isExpandable to SkeletonTable

---
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/Skeleton/SkeletonTableExpandableExample.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import SkeletonTable from '@patternfly/react-core/dist/js/components/Skeleton/SkeletonTable';
+
+export const SkeletonTableExample: React.FC = () => <SkeletonTable rowSize={10} columns={[ 'first', 'second' ]} isExpandable />;
--- packages/module/src/SkeletonTable/SkeletonTable.tsx
+++ packages/module/src/SkeletonTable/SkeletonTable.tsx
@@ -1,38 +1,80 @@
-import React, { ReactNode } from 'react';
-import { Caption, Table, TableProps, TableVariant, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import React from 'react';
+import {
+  Caption,
+  Table,
+  TableProps,
+  TableVariant,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  RowSelectVariant
+} from '@patternfly/react-table';
 import { Skeleton } from '@patternfly/react-core';
 
-export type SkeletonTableProps = Omit<TableProps, 'ref'> & {
+export interface SkeletonTableProps extends Omit<TableProps, 'ref'> {
   /** Indicates the table variant */
   variant?: TableVariant;
+  /** Flag indicating if the table should have borders */
+  borders?: boolean;
   /** The number of rows the skeleton table should contain */
   rows?: number;
   /** Any captions that should be added to the table */
-  caption?: ReactNode;
+  caption?: React.ReactNode;
   /** Custom OUIA ID */
   ouiaId?: string | number;
-} & (
-    | {
-        columns: ReactNode[];
-      }
-    | {
-        numberOfColumns: number;
-      }
-  );
-
-
-function hasCustomColumns(props: Record<string, any>): props is SkeletonTableProps & {
-  columns: ReactNode[];
-} {
-  return Array.isArray(props.columns);
+  /** Flag indicating if the table is selectable */
+  isSelectable?: boolean;
+  /** Flag indicating if the table is expandable */
+  isExpandable?: boolean;
+  /** Determines if the row is selectable using radio or checkbox */
+  selectVariant?: RowSelectVariant;
+  /** Custom columns for the table */
+  columns?: string[] | React.ReactElement<typeof Th>[];
+  /** Number of columns in the table */
+  numberOfColumns?: number;
 }
 
-const SkeletonTable: React.FunctionComponent<SkeletonTableProps> = (props: SkeletonTableProps) => {
-  const { variant, rows = 5, caption, ouiaId = 'SkeletonTable', ...rest } = props;
-  const rowCells = hasCustomColumns(props) ? props.columns.length : props.numberOfColumns;
+const SkeletonTable: React.FunctionComponent<SkeletonTableProps> = ({
+  variant,
+  borders = true,
+  rows = 5,
+  caption,
+  ouiaId = 'SkeletonTable',
+  isSelectable,
+  isExpandable,
+  selectVariant = RowSelectVariant.checkbox,
+  columns,
+  numberOfColumns,
+  ...rest
+}: SkeletonTableProps) => {
+  const hasCustomColumns = Array.isArray(columns);
+  const rowCells = hasCustomColumns ? columns?.length ?? 0 : numberOfColumns;
   const rowArray = [ ...new Array(rowCells) ];
+
   const bodyRows = [ ...new Array(rows) ].map((_, rowIndex) => (
     <Tr key={rowIndex} ouiaId={`${ouiaId}-tr-${rowIndex}`}>
+      {isExpandable && (
+        <Td
+          data-ouia-component-id={`${ouiaId}-td-expand-${rowIndex}`}
+          expand={{
+            rowIndex,
+            isExpanded: false
+          }}
+        />
+      )}
+      {isSelectable && (
+        <Td
+          data-ouia-component-id={`${ouiaId}-td-select-${rowIndex}`}
+          select={{
+            rowIndex,
+            isSelected: false,
+            isDisabled: true,
+            variant: selectVariant
+          }}
+        />
+      )}
       {rowArray.map((_, colIndex) => (
         <Td key={colIndex} data-ouia-component-id={`${ouiaId}-td-${rowIndex}-${colIndex}`}>
           <Skeleton />
@@ -42,12 +84,22 @@ const SkeletonTable: React.FunctionComponent<SkeletonTableProps> = (props: Skele
   ));
 
   return (
-    <Table aria-label="Loading" variant={variant} ouiaId={ouiaId} {...rest}>
+    <Table aria-label="Loading" variant={variant} borders={borders} ouiaId={ouiaId} {...rest}>
       {caption && <Caption>{caption}</Caption>}
       <Thead data-ouia-component-id={`${ouiaId}-thead`}>
         <Tr ouiaId={`${ouiaId}-tr-head`}>
-          {hasCustomColumns(props)
-            ? props.columns.map((c, index) => <Th key={index} data-ouia-component-id={`${ouiaId}-th-${index}`}>{c}</Th>)
+          {isExpandable && <Th key="expand" />}
+          {isSelectable && <Th key="select" />}
+          {hasCustomColumns
+            ? columns?.map((c, index) =>
+              typeof c === 'string' ? (
+                <Th key={index} data-ouia-component-id={`${ouiaId}-th-${index}`}>
+                  {c}
+                </Th>
+              ) : (
+                React.cloneElement(c, { key: index, 'data-ouia-component-id': `${ouiaId}-th-${index}` })
+              )
+            )
             : rowArray.map((_, index) => (
               <Th key={index} data-ouia-component-id={`${ouiaId}-th-${index}`}>
                 <Skeleton />
