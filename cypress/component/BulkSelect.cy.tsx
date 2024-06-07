1adc87b10c766e735220716bb5e1f4e62bb83c74
Filip Hlavac <filip.hlavac01@gmail.com>
2024-06-07 15:59:38 +0200

    Update the cypress tests

--- cypress/component/BulkSelect.cy.tsx
+++ cypress/component/BulkSelect.cy.tsx
@@ -5,12 +5,13 @@ interface DataItem {
   name: string
 };
 
-const BulkSelectTestComponent = ({ canSelectAll }: Omit<BulkSelectProps, 'onSelect' | 'selectedCount' >) => {
+const BulkSelectTestComponent = ({ canSelectAll, isDataPaginated }: Omit<BulkSelectProps, 'onSelect' | 'selectedCount' >) => {
   const [ selected, setSelected ] = useState<DataItem[]>([]);
 
   const allData = [ { name: '1' }, { name: '2' }, { name: '3' }, { name: '4' }, { name: '5' }, { name: '6' } ];
   const pageData = [ { name: '1' }, { name: '2' }, { name: '3' }, { name: '4' }, { name: '5' } ];
   const pageDataNames = pageData.map((item) => item.name);
+  const pageSelected = pageDataNames.every(item => selected.find(selectedItem => selectedItem.name === item));
 
   const handleBulkSelect = (value: BulkSelectValue) => {
     value === BulkSelectValue.none && setSelected([]);
@@ -20,19 +21,20 @@ const BulkSelectTestComponent = ({ canSelectAll }: Omit<BulkSelectProps, 'onSele
 
   return (
     <BulkSelect
+      isDataPaginated={isDataPaginated}
       canSelectAll={canSelectAll}
       pageCount={pageData.length}
       totalCount={allData.length}
       selectedCount={selected.length}
-      pageSelected={selected.length > 0 && selected.length <= 5}
-      pagePartiallySelected={selected.length > 0 && selected.length < 5}
+      pageSelected={pageSelected}
+      pagePartiallySelected={pageDataNames.some(item => selected.find(selectedItem => selectedItem.name === item)) && !pageSelected}
       onSelect={handleBulkSelect}
     />
   );
 };
 
 describe('BulkSelect', () => {
-  it('renders the bulk select', () => {
+  it('renders the bulk select without all', () => {
     cy.mount(
       <BulkSelectTestComponent />
     );
@@ -45,14 +47,14 @@ describe('BulkSelect', () => {
     cy.contains('0 selected').should('not.exist');
   });
 
-  it('renders the bulk select without all', () => {
+  it('renders the bulk select with all and without page', () => {
     cy.mount(
-      <BulkSelectTestComponent canSelectAll />
+      <BulkSelectTestComponent canSelectAll isDataPaginated={false} />
     );
     cy.get('[data-ouia-component-id="BulkSelect-checkbox"]').should('exist');
     cy.get('[data-ouia-component-id="BulkSelect-toggle"]').click();
     cy.get('[data-ouia-component-id="BulkSelect-select-all"]').should('exist');
-    cy.get('[data-ouia-component-id="BulkSelect-select-page"]').should('exist');
+    cy.get('[data-ouia-component-id="BulkSelect-select-page"]').should('not.exist');
     cy.get('[data-ouia-component-id="BulkSelect-select-none"]').should('exist');
   
     cy.contains('0 selected').should('not.exist');
@@ -76,7 +78,7 @@ describe('BulkSelect', () => {
     // Select none
     cy.get('[data-ouia-component-id="BulkSelect-toggle"]').first().click({ force: true });
     cy.get('[data-ouia-component-id="BulkSelect-select-none"]').first().click();
-    cy.contains('0 selected').should('not.exist');
+    cy.get('input[type="checkbox"]').should('not.be.checked');
   
     // Select all
     cy.get('[data-ouia-component-id="BulkSelect-toggle"]').first().click({ force: true });
