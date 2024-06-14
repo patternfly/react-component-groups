9c26300ec002c92aca55fce12b85d2d25d3ae62c
Filip Hlavac <filip.hlavac01@gmail.com>
2024-06-14 14:36:31 +0200

    Migrate ErrorState & ErrorBoundary

--- cypress/component/ErrorBoundary.cy.tsx
+++ cypress/component/ErrorBoundary.cy.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@patternfly/react-core';
 import ErrorBoundary from '../../packages/module/dist/dynamic/ErrorBoundary';
 
 describe('ErrorBoundary', () => {
   it('renders the ErrorBoundary ', () => {
-    cy.mount(<ErrorBoundary headerTitle="My app header" errorTitle="Something wrong happened"/>)
-    cy.get('div h1').should('have.text', 'Something wrong happened');
+    cy.mount(<ErrorBoundary headerTitle="My app header" errorTitle="Something wrong happened">
+      <Button ouiaId="test-content">Test</Button>
+    </ErrorBoundary>);
+    cy.get('[data-ouia-component-id="test-content"]').contains('Test');
+    cy.get('[data-ouia-component-id="ErrorState"]').should('not.exist');
   });
 })
