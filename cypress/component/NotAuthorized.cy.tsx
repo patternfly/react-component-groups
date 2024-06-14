d3c3c7c049e05d2f35db2314e210a9711eeb0198
Filip Hlavac <filip.hlavac01@gmail.com>
2024-06-14 14:38:23 +0200

    Migrate NotAuthorized

--- cypress/component/NotAuthorized.cy.tsx
+++ cypress/component/NotAuthorized.cy.tsx
@@ -5,28 +5,27 @@ import NotAuthorized from '../../packages/module/dist/dynamic/NotAuthorized';
 describe('NotAuthorized', () => {
   it('renders basic NotAuthorized', () => {
     cy.mount(<NotAuthorized 
-      description="Test text" 
+      bodyText="Test text" 
       serviceName="Test bundle"
-      prevPageButtonText="Go to previous page"
     />);
-    cy.get('div div div h5').should('have.text', 'You do not have access to Test bundle');
-    cy.get('[class="pf-v5-c-empty-state__body"').should('have.text', 'Test text');
+    cy.get('[data-ouia-component-id="NotAuthorized"]').contains('You do not have access to Test bundle');
+    cy.get('[data-ouia-component-id="NotAuthorized-body"]').contains('Test text');
   });
 
   it('renders NotAuthorized with custom action buttons', () => {
     const onClickSpy = cy.spy().as('onClickSpy');
     const primaryAction = 
-    <Button key="1" onClick={onClickSpy}>
+    <Button key="1" onClick={onClickSpy} ouiaId="test-button">
       Custom primary action
     </Button>;
     const customNotAuthorized = <NotAuthorized 
       primaryAction={primaryAction}
-      description="Test text" 
+      bodyText="Test text" 
       serviceName="Demo bundle"
       prevPageButtonText="Go to previous page"
     />
     cy.mount(customNotAuthorized);
-    const customButton = cy.get('div div div div button')
+    const customButton = cy.get('[data-ouia-component-id="test-button"]')
     customButton.should('exist');
     customButton.should('have.text', 'Custom primary action');
     customButton.click();
