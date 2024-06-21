5cffd3c1c169a9b4ea1ccbe1c42f7d9f67461775
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2024-06-21 15:45:28 +0200

    fix(docs): Fix ErrorState examples (#148)

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorState/ErrorStateFooterExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorState/ErrorStateFooterExample.tsx
@@ -3,6 +3,6 @@ import { Button } from '@patternfly/react-core';
 import ErrorState from "@patternfly/react-component-groups/dist/dynamic/ErrorState";
 
 // eslint-disable-next-line no-console
-export const BasicExample: React.FunctionComponent = () => <ErrorState errorTitle='Sample error title' errorDescription='Sample error description' customFooter={<Button variant="secondary" onClick={() => console.log("Custom button clicked")}>
+export const BasicExample: React.FunctionComponent = () => <ErrorState titleText='Sample error title' bodyText='Sample error description' customFooter={<Button variant="secondary" onClick={() => console.log("Custom button clicked")}>
 Custom action
 </Button>}/>;
