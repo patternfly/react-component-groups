5ee02316e125c34ff500a79092561035496f2896
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2024-06-21 15:45:28 +0200

    fix(docs): Fix ErrorState examples (#148)

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorState/ErrorStateExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorState/ErrorStateExample.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ErrorState from "@patternfly/react-component-groups/dist/dynamic/ErrorState";
 
-export const BasicExample: React.FunctionComponent = () => <ErrorState errorTitle='Sample error title' errorDescription='Sample error description' />;
+export const BasicExample: React.FunctionComponent = () => <ErrorState titleText='Sample error title' bodyText='Sample error description' />;
