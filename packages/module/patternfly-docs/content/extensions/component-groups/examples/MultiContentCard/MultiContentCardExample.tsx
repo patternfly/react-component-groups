0c08332cfd3774d4c079dc33cbab1d554285a7ba
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2024-07-30 10:10:34 -0400

    Update to the latest PF versions (#224)
    * Update PF dependencies
    
    * Fix MCC colors
    
    * Replace not existing PF Text components
    
    * Update snapshots

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/MultiContentCard/MultiContentCardExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/MultiContentCard/MultiContentCardExample.tsx
@@ -1,112 +1,102 @@
 import React from 'react';
 import MultiContentCard from "@patternfly/react-component-groups/dist/dynamic/MultiContentCard";
-import { Button, Card, CardHeader, CardBody, Text, TextContent, TextVariants, Icon, TextList, TextListItem, CardFooter } from '@patternfly/react-core';
+import { Button, Card, CardHeader, CardBody, Content, ContentVariants, List, ListItem, Icon, CardFooter } from '@patternfly/react-core';
 import { ArrowRightIcon, BellIcon, CogIcon, LockIcon } from '@patternfly/react-icons';
 import { createUseStyles } from 'react-jss';
 import clsx from 'clsx';
 
 const useStyles = createUseStyles({
   action: {
-    color: 'var(--pf-t--color--blue--60)',
+    color: 'var(--pf-t--global--text--color--brand--default)',
     fontSize: 'var(--pf-t--global--font--size--200)',
-  }, 
+  },
+  actionIcon: {
+    color: 'var(--pf-t--global--color--brand--default)',
+  },
   bulletPoints: {
     color: 'var(--pf-t--global--color--brand--default)',
   },
 });
-
 export const BasicExample: React.FunctionComponent = () => {
   const classes = useStyles();
 
   const cards = [
     <Card isFullHeight isPlain key="card-1">
       <CardHeader>
-        <TextContent>
-          <Text component={TextVariants.h4}>Getting Started</Text>
-        </TextContent>
+        <Content component={ContentVariants.h4}>Getting Started</Content>
       </CardHeader>
       <CardBody>
-        <TextContent>
-          <Text className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
-            <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
-              <CogIcon />
-            </Icon>
+        <Content className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
+          <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
+            <CogIcon className={classes.actionIcon} />
+          </Icon>
             Configure application
-          </Text>
-          <Text className="pf-v6-u-font-size-sm">
+        </Content>
+        <Content className="pf-v6-u-font-size-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Text>
-        </TextContent>
+        </Content>
       </CardBody>
       <CardFooter>
-        <TextContent>
-          <TextList className={clsx(classes.bulletPoints, 'pf-v6-u-font-size-sm', 'pf-v6-u-ml-0')}>
-            <TextListItem>
-              <Button variant="link" isInline>First link</Button>
-            </TextListItem>
-            <TextListItem>
-              <Button variant="link" isInline>Second link</Button>
-            </TextListItem>
-            <TextListItem>
-              <Button variant="link" isInline>Another link</Button>
-            </TextListItem>
-          </TextList>
-        </TextContent>
+        <List className={clsx(classes.bulletPoints, 'pf-v6-u-font-size-sm', 'pf-v6-u-ml-0')}>
+          <ListItem>
+            <Button variant="link" isInline>First link</Button>
+          </ListItem>
+          <ListItem>
+            <Button variant="link" isInline>Second link</Button>
+          </ListItem>
+          <ListItem>
+            <Button variant="link" isInline>Another link</Button>
+          </ListItem>
+        </List>
       </CardFooter>
     </Card>,
     <Card isFullHeight isPlain key="card-2">
       <CardBody className="pf-v6-u-pt-3xl-on-md">
-        <TextContent>
-          <Text className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
-            <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
-              <LockIcon />
-            </Icon>
+        <Content className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
+          <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
+            <LockIcon className={classes.actionIcon} />
+          </Icon>
             Configure access
-          </Text>
-          <Text>
+        </Content>
+        <Content>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </Text>
-        </TextContent>
+        </Content>
       </CardBody>
       <CardFooter>
-        <Text>
+        <Content>
           <Button variant="link" isInline>
               Learn more  
             <Icon className="pf-v6-u-ml-sm" isInline>
               <ArrowRightIcon />
             </Icon>
           </Button>
-        </Text>
+        </Content>
       </CardFooter>
     </Card>,
     <Card isFullHeight isPlain key="card-3">
       <CardHeader>
-        <TextContent>
-          <Text component={TextVariants.h4}>Next Steps</Text>
-        </TextContent>
+        <Content component={ContentVariants.h4}>Next Steps</Content>
       </CardHeader>
       <CardBody>
-        <TextContent>
-          <Text className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
-            <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
-              <BellIcon />
-            </Icon>
+        <Content className={clsx(classes.action, 'pf-v6-u-font-weight-bold', 'pf-v6-u-mb-sm')}>
+          <Icon size="md" className="pf-v6-u-pl-sm pf-v6-u-pr-md">
+            <BellIcon className={classes.actionIcon} />
+          </Icon>
             Configure notifications
-          </Text>
-          <Text>
+        </Content>
+        <Content>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Text>
-        </TextContent>
+        </Content>
       </CardBody>
       <CardFooter>
-        <Text>
+        <Content>
           <Button variant="link" isInline>
               Learn more  
             <Icon className="pf-v6-u-ml-sm" isInline>
               <ArrowRightIcon />
             </Icon>
           </Button>
-        </Text>
+        </Content>
       </CardFooter>
     </Card>
   ];
