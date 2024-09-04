867c3d06c31a9bb218d041709a7a1a9baccf0f85
Alexandra Ferdinand <aferdina@redhat.com>
2024-09-04 16:15:24 -0400

    init ansible icon updates

---
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/Ansible/AnsibleTechnologyExample.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import AnsibleTechnology from '@patternfly/react-component-groups/dist/dynamic/Ansible';
+
+export const BasicExample: React.FunctionComponent = () => (
+  <AnsibleTechnology />
+);
