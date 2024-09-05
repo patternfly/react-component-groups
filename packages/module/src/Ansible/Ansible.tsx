549029d833cc7ce3f11cb5e38f9e39628e1eab96
aferd <aferdina@redhat.com>
2024-09-05 11:30:03 -0400

    lint fix

--- packages/module/src/Ansible/Ansible.tsx
+++ packages/module/src/Ansible/Ansible.tsx
@@ -78,24 +78,24 @@ const Ansible: React.FunctionComponent<AnsibleProps> = ({ unsupported, RHAAPIcon
   return (
     <React.Fragment>
       {RHAAPIcon ? (<AdIcon />) : (
-      <i className={ansibleLogoClass} title={unsupported ? "Ansible is not supported" : "Ansible supported" } data-ouia-component-id={ouiaId} {...props}>
-        <svg
-          version="1.1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 2032 2027.2"
-          style={{ enableBackground: 'new 0 0 2032 2027.2' } as React.CSSProperties}
-        >
-          <path
-            className="st0"
-            d="M2030.8,1014.8c0,559.2-453.3,1012.4-1012.4,1012.4C459.2,2027.2,5.9,1574,5.9,1014.8
+        <i className={ansibleLogoClass} title={unsupported ? "Ansible is not supported" : "Ansible supported" } data-ouia-component-id={ouiaId} {...props}>
+          <svg
+            version="1.1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 2032 2027.2"
+            style={{ enableBackground: 'new 0 0 2032 2027.2' } as React.CSSProperties}
+          >
+            <path
+              className="st0"
+              d="M2030.8,1014.8c0,559.2-453.3,1012.4-1012.4,1012.4C459.2,2027.2,5.9,1574,5.9,1014.8
                         C5.9,455.7,459.2,2.4,1018.3,2.4C1577.5,2.4,2030.8,455.7,2030.8,1014.8 M1035.4,620.9l262,646.6L901.7,955.8L1035.4,620.9
                         L1035.4,620.9z M1500.8,1416.5l-403-969.9c-11.5-28-34.5-42.8-62.4-42.8c-28,0-52.7,14.8-64.2,42.8L528.9,1510.4h151.3l175.1-438.6
                         l522.5,422.1c21,17,36.2,24.7,55.9,24.7c39.5,0,74-29.6,74-72.3C1507.7,1439.4,1505.3,1428.3,1500.8,1416.5L1500.8,1416.5z"
-          />
-          {unsupported ? unsupportedSlash : null}
-        </svg>
-      </i>
+            />
+            {unsupported ? unsupportedSlash : null}
+          </svg>
+        </i>
       ) }
     </React.Fragment>
   );
