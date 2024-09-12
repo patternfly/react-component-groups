84d43f582160229690a1a0132817fdb16bb2721a
Filip Hlavac <filip.hlavac01@gmail.com>
2024-09-12 11:52:42 +0200

    fix(nav): Resolve build warnings

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/DetailsPage/DetailsPage.md
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/DetailsPage/DetailsPage.md
@@ -28,7 +28,6 @@ sourceLink: https://github.com/patternfly/react-component-groups/blob/main/packa
 ---
 
 import { ActionMenu } from '@patternfly/react-component-groups';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { CheckCircleIcon } from '@patternfly/react-icons';
 import HorizontalNav from '@patternfly/react-component-groups/dist/dynamic/HorizontalNav';
 import DetailsPage from '@patternfly/react-component-groups/dist/dynamic/DetailsPage';
--- packages/module/patternfly-docs/content/extensions/component-groups/examples/DetailsPage/DetailsPageHeaderExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/DetailsPage/DetailsPageHeaderExample.tsx
@@ -1,61 +1,58 @@
 /* eslint-disable no-console */
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { CheckCircleIcon } from '@patternfly/react-icons';
 import DetailsPageHeader from '@patternfly/react-component-groups/dist/dynamic/DetailsPageHeader';
 import { Breadcrumb, BreadcrumbItem } from '@patternfly/react-core';
 
 export const BasicExample: React.FunctionComponent = () => (
-  <Router>
-    <DetailsPageHeader
-      breadcrumbs={ 
-        <Breadcrumb>
-          <BreadcrumbItem
-            to="/resources"
-            key="resources"
-          >
+  <DetailsPageHeader
+    breadcrumbs={ 
+      <Breadcrumb>
+        <BreadcrumbItem
+          to="/resources"
+          key="resources"
+        >
             Resources
-          </BreadcrumbItem>
-          <BreadcrumbItem
-            isActive
-            to="/resources/example-resource"
-            key="resources-example"
-          >
+        </BreadcrumbItem>
+        <BreadcrumbItem
+          isActive
+          to="/resources/example-resource"
+          key="resources-example"
+        >
             Resource details
-          </BreadcrumbItem>
-        </Breadcrumb>
-      }
-      pageHeading={{
-        title: 'example-resource',
-        label: {
-          children: 'Ready',
-          icon: <CheckCircleIcon color="#3E8635" />
-        },
-      }}
-      actionButtons={[
+        </BreadcrumbItem>
+      </Breadcrumb>
+    }
+    pageHeading={{
+      title: 'example-resource',
+      label: {
+        children: 'Ready',
+        icon: <CheckCircleIcon color="#3E8635" />
+      },
+    }}
+    actionButtons={[
+      {
+        children: 'Primary action',
+        // eslint-disable-next-line no-console
+        onClick: () => console.log('Primary action clicked'),
+        tooltip: 'Click me!',
+      },
+    ]}
+    actionMenu={{
+      actions: [
         {
-          children: 'Primary action',
-          // eslint-disable-next-line no-console
-          onClick: () => console.log('Primary action clicked'),
-          tooltip: 'Click me!',
+          children: 'Edit resource',
+          itemId: 'details-page-header-action-menu-example-1',
+          onClick: () => console.log('Edit resource clicked'),
         },
-      ]}
-      actionMenu={{
-        actions: [
-          {
-            children: 'Edit resource',
-            itemId: 'details-page-header-action-menu-example-1',
-            onClick: () => console.log('Edit resource clicked'),
-          },
-          {
-            children: 'Delete resource',
-            itemId: 'details-page-header-action-menu-example-2',
-            onClick: () => console.log('Delete resource clicked'),
-            isDisabled: true,
-          },
-        ],
-        id: 'details-page-header-action-menu-example',
-      }}
-    />
-  </Router>
+        {
+          children: 'Delete resource',
+          itemId: 'details-page-header-action-menu-example-2',
+          onClick: () => console.log('Delete resource clicked'),
+          isDisabled: true,
+        },
+      ],
+      id: 'details-page-header-action-menu-example',
+    }}
+  />
 );
