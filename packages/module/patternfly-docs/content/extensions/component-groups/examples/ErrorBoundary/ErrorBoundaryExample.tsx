5415183aa25a1ad8f0cefeab34b28a59a318b561
Filip Hlavac <filip.hlavac01@gmail.com>
2024-09-12 14:33:29 +0200

    fix(build): Fix ErrorBoundary example

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorBoundary/ErrorBoundaryExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorBoundary/ErrorBoundaryExample.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Title, Button, Card, CardBody, CardFooter, CardHeader } from '@patternfly/react-core';
 import ErrorBoundary from '@patternfly/react-component-groups/dist/dynamic/ErrorBoundary';
 
 export const BasicExample: React.FunctionComponent = () => {
+  const [ hasError, setHasError ] = useState(false);
+
   const Surprise = () => {
-    throw new Error('but a welcome one');
+    if (hasError) {
+      throw new Error('but a welcome one');
+    }
+    return (
+      <Card>
+        <CardHeader>
+          <Title headingLevel='h2'>Demo content wrapped in error boundary</Title>
+        </CardHeader>
+        <CardBody>This is a demo content that may throw an error:</CardBody>
+        <CardFooter>
+          <Button className='pf-v6-u-mt-lg' variant='danger' onClick={() => setHasError(true)}>
+            Click to throw an error
+          </Button>
+        </CardFooter>
+      </Card>
+    )
   };
 
-  return (<ErrorBoundary headerTitle="My app header" errorTitle="Something wrong happened">
-    <Surprise />
-  </ErrorBoundary>);
-}
+  return (
+    <>
+      <ErrorBoundary errorTitle="Something wrong happened">
+        <Surprise />
+      </ErrorBoundary>
+      {
+        hasError && (
+          <Button className='pf-v6-u-mt-lg' variant='secondary' onClick={() => window.location.reload()}>
+            Reload page
+          </Button>
+        )
+      }
+    </>
+  );
+};
--- packages/module/patternfly-docs/content/extensions/component-groups/examples/ErrorBoundary/ErrorBoundaryNoExample.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ErrorBoundary from '@patternfly/react-component-groups/dist/dynamic/ErrorBoundary';
-
-export const BasicExample: React.FunctionComponent = () => (<ErrorBoundary headerTitle={'My app header'}>
-  <div>My app content</div>
-</ErrorBoundary>);
-
