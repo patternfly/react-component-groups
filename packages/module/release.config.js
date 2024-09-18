175ea7364285bdb76f7906dd9dc7ff0a4c9df89d
Donald Labaj <dlabaj@gmail.com>
2024-09-18 16:42:36 -0400

    fix: Updated branch config.

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   branches: [ 
     'do-not-delete',
-    { name: 'v5',   prerelease: true, channel: 'prerelease-v5', range: '5.x' } ],
+    { name: 'v5', prerelease: true, channel: 'prerelease-v5', range: '5.x' } ],
   analyzeCommits: {
     preset: 'angular'
   },
