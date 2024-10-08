be68baeb98184e64926f03797431c2adfb238804
Nicole Thoen <nthoen@redhat.com>
2024-10-07 21:51:18 -0400

    fix: try removing range and force only patches instead

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -2,17 +2,31 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v5', channel: 'prerelease-v5', range: '5.x' }
+    { name: 'v5', channel: 'prerelease-v5' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          {"type": "fix", "release": "patch"},
+          {"type": "docs", "release": "patch"},
+          {"type": "refactor", "release": "patch"},
+          {"type": "chore", "release": "patch"},
+          {"type": "style", "release": "patch"},
+          {"type": "feat", "release": "patch"},
+          {"type": "perf", "release": "patch"}
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'v${version}',
+  tagFormat: 'prerelease-v${version}',
   dryRun: true
 };
