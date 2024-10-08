30fdc7045883f6027be096b1e7ae886f026a147b
Nicole Thoen <nthoen@redhat.com>
2024-10-08 11:55:57 -0400

    fix: Update release.config.js

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -2,27 +2,13 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v5', channel: 'prerelease-v5' }
+    { name: 'v5', channel: 'prerelease-v5', range: '5.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        'preset': 'angular',
-        'releaseRules': [
-          { 'type': 'fix', 'release': 'patch' },
-          { 'type': 'docs', 'release': 'patch' },
-          { 'type': 'refactor', 'release': 'patch' },
-          { 'type': 'chore', 'release': 'patch' },
-          { 'type': 'style', 'release': 'patch' },
-          { 'type': 'feat', 'release': 'patch' },
-          { 'type': 'perf', 'release': 'patch' }
-        ]
-      }
-    ],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/npm'
