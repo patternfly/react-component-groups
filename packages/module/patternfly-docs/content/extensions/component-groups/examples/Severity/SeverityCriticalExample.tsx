87a6f71bf141553475689fc9a1bdad7ca97ecc36
Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
2024-10-11 00:09:31 +0200

    Update packages/module/patternfly-docs/content/extensions/component-groups/examples/Severity/SeverityCriticalExample.tsx

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/Severity/SeverityCriticalExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/Severity/SeverityCriticalExample.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import Severity, { SeverityType } from '@patternfly/react-component-groups/dist/dynamic/Severity';
 
 export const BasicExample: React.FunctionComponent = () => (
-
   <Severity severity={SeverityType.critical} label="Critical" />
-
 );
