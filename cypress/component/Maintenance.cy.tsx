f8dd98e50853254c4d67a27a6fce8b5ab34be59e
aferd <aferdina@redhat.com>
2024-10-31 11:45:00 -0400

    adjust cypress test

--- cypress/component/Maintenance.cy.tsx
+++ cypress/component/Maintenance.cy.tsx
@@ -6,6 +6,6 @@ describe('Maintenance', () => {
     cy.mount(<Maintenance />)
     cy.get('[data-ouia-component-id="Maintenance"]').should('exist');
     cy.get('[data-ouia-component-id="Maintenance"]').contains('Maintenance in progress');
-    cy.get('[data-ouia-component-id="Maintenance-body"]').contains('We are currently undergoing scheduled maintenance and will be unavailable from 10am to 12am UTC (6am-8am EST). For more information please visit status.redhat.com.');
+    cy.get('[data-ouia-component-id="Maintenance-body"]').contains('We are currently undergoing scheduled maintenance and will be unavailable from 6am to 8am UTC. For more information please visit status.redhat.com.');
   });
 });
--- packages/module/patternfly-docs/content/extensions/component-groups/examples/Maintenance/MaintenanceCustomExample.tsx
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/Maintenance/MaintenanceCustomExample.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Maintenance from '@patternfly/react-component-groups/dist/dynamic/Maintenance'
 
 export const BasicExample: React.FunctionComponent = () => (
-  <Maintenance bodyText='We are currently undergoing scheduled maintenance and will be unavailable from' customFooter='Please visit' redirectLinkUrl='http://patternfly.com' redirectLinkText='here.' startTime='6am' endTime='8am' timeZone='EST' />
+  <Maintenance bodyText='We are currently undergoing scheduled maintenance and will be unavailable from' customFooter='Please visit' redirectLinkUrl='http://patternfly.com' redirectLinkText='here.' startTime='6am' endTime='8am' timeZone='UTC' />
 );
