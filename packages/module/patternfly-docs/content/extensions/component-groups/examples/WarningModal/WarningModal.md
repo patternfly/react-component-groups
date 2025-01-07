a29469cfde6d26eea6fac1d9479ca22dc14785ca
Zein Sleiman <zsleiman@redhat.com>
2025-01-07 14:07:30 -0600

    feat(WarningModal): add text confirmation option

--- packages/module/patternfly-docs/content/extensions/component-groups/examples/WarningModal/WarningModal.md
+++ packages/module/patternfly-docs/content/extensions/component-groups/examples/WarningModal/WarningModal.md
@@ -49,3 +49,11 @@ To confirm that a user wishes to initiate a selected action, you can add a check
 
 ```
 
+
+### Warning modal with a text confirmation
+
+To confirm that a user wishes to initiate a selected action, you can add a text confirmation.
+
+```js file="./WarningModalTextConfirmationExample.tsx"
+
+```
--- packages/module/src/WarningModal/WarningModal.tsx
+++ packages/module/src/WarningModal/WarningModal.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, ButtonVariant, Checkbox } from '@patternfly/react-core';
+import React, { ReactNode, useState } from 'react';
+import { Button, ButtonVariant, Checkbox, Flex, FlexItem, Stack, StackItem, TextInput, TextInputProps } from '@patternfly/react-core';
 import { ModalProps, Modal, ModalVariant } from '@patternfly/react-core/deprecated';
 
 /** extends ModalProps */
@@ -13,11 +13,17 @@ export interface WarningModalProps extends Omit<ModalProps, 'ref'> {
   /** Whether modal requires a checkbox before confirming */
   withCheckbox?: boolean;
   /** Custom checkbox label */
-  checkboxLabel?: string;
+  checkboxLabel?: ReactNode;
   /** Visual variant of the confirm button */
   confirmButtonVariant?: ButtonVariant;
   /** Custom OUIA ID */
   ouiaId?: string | number;
+  /** Whether modal requires a text confirmation */
+  textConfirmation?: TextInputProps;
+  /** Label for the text confirmation */
+  textConfirmationLabel?: (deleteName?: string) => ReactNode;
+  /** Text the user should type to confirm selection when using textConfirmation */
+  deleteName?: string;
 }
 
 const WarningModal: React.FunctionComponent<WarningModalProps> = ({
@@ -33,9 +39,32 @@ const WarningModal: React.FunctionComponent<WarningModalProps> = ({
   checkboxLabel='I understand that this action cannot be undone',
   confirmButtonVariant = ButtonVariant.primary,
   ouiaId = 'WarningModal',
+  textConfirmation,
+  textConfirmationLabel = (deleteName) => (
+    <>Type <strong>{deleteName} </strong> to confirm deletion:</>
+  ),
+  deleteName,
   ...props
 }: WarningModalProps) => {
   const [ checked, setChecked ] = useState(false);
+  const [ inputValue, setInputValue ] = React.useState('');
+
+  const deleteNameSanitized = React.useMemo(() => deleteName?.trim().replace(/\s+/g, ' '), [ deleteName ]);
+
+  const textConfirmed = textConfirmation ? inputValue.trim() === deleteNameSanitized : true;
+
+  const isConfirmButtonDisabled = () => {
+    if(withCheckbox && textConfirmation) {
+      return !checked || !textConfirmed;
+    }
+    if(withCheckbox && !textConfirmation) {
+      return !checked;
+    }
+    if(!withCheckbox && textConfirmation) {
+      return !textConfirmed;
+    }
+    else {return false;}
+  }
 
   return (
     <Modal
@@ -52,8 +81,9 @@ const WarningModal: React.FunctionComponent<WarningModalProps> = ({
           onClick={() => {
             onConfirm?.();
             setChecked(false);
+            setInputValue('');
           }}
-          isDisabled={withCheckbox && !checked}
+          isDisabled={isConfirmButtonDisabled()}
         >
           {confirmButtonLabel}
         </Button>,
@@ -69,17 +99,35 @@ const WarningModal: React.FunctionComponent<WarningModalProps> = ({
       ouiaId={ouiaId}
       {...props}
     >
-      {children}
-      {withCheckbox ? (
-        <Checkbox
-          isChecked={checked}
-          onChange={(_event, value) => setChecked(value)}
-          label={checkboxLabel}
-          id="warning-modal-check"
-          className="pf-v6-u-mt-lg"
-          ouiaId={`${ouiaId}-confirm-checkbox`}
-        />
-      ) : null}
+      <Stack hasGutter>
+        <StackItem>{children}</StackItem>
+        <StackItem>
+          {textConfirmation ? (
+            <Flex direction={{ default: 'column' }} spaceItems={{ default: 'spaceItemsSm' }}>
+              <FlexItem>
+                {textConfirmationLabel && textConfirmationLabel(deleteNameSanitized)}
+              </FlexItem>
+              <TextInput
+                ouiaId={`${ouiaId}-confirmation-text-input`}
+                value={inputValue}
+                onChange={(_e, value) => setInputValue(value)}
+              />
+            </Flex>
+          ) : null}          
+        </StackItem>
+        <StackItem>
+          {withCheckbox ? (
+            <Checkbox
+              isChecked={checked}
+              onChange={(_event, value) => setChecked(value)}
+              label={checkboxLabel}
+              id="warning-modal-check"
+              className="pf-v6-u-mt-lg"
+              ouiaId={`${ouiaId}-confirm-checkbox`}
+            />
+          ) : null}
+        </StackItem>
+      </Stack>
     </Modal>
   )
 
