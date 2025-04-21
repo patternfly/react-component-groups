0d6c8fc138841213e5127eae364ff8e227acf5c1
Donald Labaj <donald.labaj@gmail.com>
2025-04-21 14:04:57 -0400

    fix: Updated to release to prerelase-bugfix branch.

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -2,17 +2,28 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v5', channel: 'prerelease-v5' }
+    { name: 'v5', channel: 'prerelease-v5' },
+    { name: '6.2.x', channel: 'prerelease-bugfix', range: '6.2.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    '@semantic-release/npm'
-  ],
-  tagFormat: 'prerelease-v${version}',
+    '@semantic-release/npm',
+  ]
+,
+  tagFormat: 'patch-v${version}',
   dryRun: false
 };
