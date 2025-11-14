7200043bd7e6befbb70b83e3dcc789b53dcf4d19
nicolethoen <nthoen@redhat.com>
2025-11-14 09:26:35 -0500

    fix failing cypress test

--- cypress/e2e/CloseButton.spec.cy.ts
+++ cypress/e2e/CloseButton.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Test the close button', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8006/component-groups/close-button', { onBeforeLoad: (win) => {cy.stub(win.console, 'log').as('consoleLog');}, });
+    cy.visit('http://localhost:8006/extensions/component-groups/close-button', { onBeforeLoad: (win) => {cy.stub(win.console, 'log').as('consoleLog');}, });
     cy.wait(1000);
 
     cy.get('[data-test-id="close-button-example"]').click();
